61b93494f7e57166214790d88124d5d7ff2f686e
Cleyrauz <cleyra.uzcategui@gmail.com>
2018-05-16 20:40:50 +0100

    Added three ways to return Falkirk High

--- exercise_a.rb
+++ exercise_a.rb
@@ -6,6 +6,10 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston",
   stops.insert(4, 'Polmont')
   stops.delete_at(6)
   stops.delete_at(2)
-
+  p stops
   array_length = stops.length
   p "There are #{array_length} stops!"
+  p stops[2]
+  p stops[-5]
+  return_falkikrk_high = stops.at(2)
+  p return_falkikrk_high
