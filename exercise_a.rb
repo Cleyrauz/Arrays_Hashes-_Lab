cc7644287b3190d6b8571c1e2a407e018441be54
Cleyrauz <cleyra.uzcategui@gmail.com>
2018-05-16 20:21:45 +0100

    Defined how many stops there are

--- exercise_a.rb
+++ exercise_a.rb
@@ -1,10 +1,11 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston",
   "Haymarket" ]
 
-stops.push("Edinburgh Waverley")
-stops.unshift("Glasgow Queen St")
-stops.insert(4, 'Polmont')
-stops.delete_at(6)
-stops.delete_at(2)
-# 
-p stops
+  stops.push("Edinburgh Waverley")
+  stops.unshift("Glasgow Queen St")
+  stops.insert(4, 'Polmont')
+  stops.delete_at(6)
+  stops.delete_at(2)
+
+  array_length = stops.length
+  p "There are #{array_length} stops!"
