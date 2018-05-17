5ec0036459d53be706d810b7f340d9fe0328b7ed
Cleyrauz <cleyra.uzcategui@gmail.com>
2018-05-17 07:33:47 +0100

    Changed  the capital of Wales from Swansea to Cardiff

---
+++ exercise_c.rb
@@ -0,0 +1,22 @@
+united_kingdom = [
+  {
+    name: "Scotland",
+    population: 5295000,
+    capital: "Edinburgh"
+  },
+  {
+    name: "Wales",
+    population: 3063000,
+    capital: "Swansea"
+  },
+  {
+    name: "England",
+    population: 53010000,
+    capital: "London"
+  }
+]
+
+# Change the capital of Wales from "Swansea" to "Cardiff".
+
+p united_kingdom[1][:capital] = "Cardiff"
+p united_kingdom
